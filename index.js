58e5272e2588389c88495703315703938642e043
arafatrahat94 <arafatrahat94@gmail.com>
2024-01-31 12:14:09 +0600

    express is installed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const sqlite3 = require("sqlite3");
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 // Enable CORS for all routes
 app.use(cors());
