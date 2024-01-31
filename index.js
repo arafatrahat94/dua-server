5098220cdc020ea17da0bef69be492093c76bad8
arafatrahat94 <arafatrahat94@gmail.com>
2024-01-31 12:01:42 +0600

    connecting to repository

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+const port = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+const dbFile = "dua_main.sqlite";
+const db = new sqlite3.Database(dbFile);
+const categoryQuery = "SELECT * FROM category";
+
+app.get("/categoryData", (req, res) => {
+  db.all(categoryQuery, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    res.json(rows);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
