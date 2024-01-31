f944a4355d0759a4aace2c5697c8f8e89433ac64
arafatrahat94 <arafatrahat94@gmail.com>
2024-01-31 17:33:04 +0600

    added api for dua and sub category

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ const dbFile = path.resolve(__dirname, "dua_main.sqlite");
 
 const db = new sqlite3.Database(dbFile);
 const categoryQuery = "SELECT * FROM category";
+const subCategoryQuery = "SELECT * FROM sub_category";
+const duaQuery = "SELECT * FROM dua";
 
+app.get("/dua", (req, res) => {
+  db.all(duaQuery, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    res.json(rows);
+  });
+});
+app.get("/sub_category", (req, res) => {
+  db.all(subCategoryQuery, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    res.json(rows);
+  });
+});
 app.get("/categoryData", (req, res) => {
   db.all(categoryQuery, (err, rows) => {
     if (err) {
